cb7360e9d2fa4bc47fe54fc71f1e5c15c71d72c8
kazuhiro kotsuka <kazuhiro.kotsuka@sacn.co.jp>
2022-05-15 19:07:57 +0900

    add login

---
+++ resources/views/regist/complate.blade.php
@@ -0,0 +1,10 @@
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>登録完了画面</title>
+</head>
+<body>
+    <p>{{ $user->name }}さんを登録しました。</p>
+    <p><a href="/home">ホームに戻る</a></p>
+</body>
+</html>
