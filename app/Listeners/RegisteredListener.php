4c2e6be18f2a6c70d27c83d7aef89c031ebf1ba4
kazuhiro kotsuka <kazuhiro.kotsuka@sacn.co.jp>
2022-06-08 22:39:06 +0900

    2-4 contract

--- app/Listeners/RegisteredListener.php
+++ app/Listeners/RegisteredListener.php
@@ -1,21 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Listeners;
 
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Mail\Mailer;
+use App\Models\User;
 
 class RegisteredListener
 {
+    private $mailer;
+    private $eloquent;
+
     /**
      * Create the event listener.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Mailer $mailer, User $eloquent)
     {
-        //
+        $this->mailer = $mailer;
+        $this->eloquent = $eloquent;
     }
 
     /**
@@ -26,6 +32,9 @@ class RegisteredListener
      */
     public function handle(Registered $event)
     {
-        //
+        $user = $this->eloquent->findOrFail($event->user->getAuthIdentifier());
+        $this->mailer->raw('会員登録完了しました', function($message) use ($user){
+            $message->subject('会員登録メール')->to($user->email);
+        });
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\BlowfishEncrypter;
+use illuminate\Encryption\MissingAppKeyException;
 use Illuminate\Support\ServiceProvider;
+use illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,36 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //2-4 コントラクト
+        // $this->app->singleton(
+        //     'encrypter',
+        //     function($app){
+        //         $config = $app->make('config')->get('app');
+
+        //         return new BlowfishEncrypter($this->parseKey($config));
+        //     }
+        // );
+    }
+
+    protected function parseKey(array $config)
+    {
+        if(Str::startsWith($key = $this->key($config), $prefix = 'base64')){
+            $key = base64_decode(Str::after($key, $prefix));
+        }
+
+        return $key;
+    }
+
+    protected function key(array $config)
+    {
+        return tap(
+            $config['key'],
+            function($key){
+                if(empty($key)){
+                    throw new MissingAppKeyException;
+                }
+            }
+        );
     }
 
     /**
